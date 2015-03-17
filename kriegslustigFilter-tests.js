549ff6d895027cba1278a1e24c029190bf5eee43
Kriegslustig <github@ls7.ch>
2015-03-17 18:39:35 +0100

    Add an unreactive test for getItems

--- kriegslustigFilter-tests.js
+++ kriegslustigFilter-tests.js
@@ -1,10 +1,21 @@
 Tinytest.add('init', function (test) {
   var TestCollection = new Mongo.Collection()
-  var kriegslustigFilterTestInstance = Object.create(KriegslustigFilter)
+, kriegslustigFilterTestInstance = Object.create(KriegslustigFilter)
   kriegslustigFilterTestInstance.collection = TestCollection
   kriegslustigFilterTestInstance.init()
   TestCollection.insert({
     something: true
   })
   test.equal(kriegslustigFilterTestInstance._items[0].something, true, 'Initiation failed');
+});
+
+Tinytest.add('getItems', function (test) {
+  var TestCollection = new Mongo.Collection()
+, kriegslustigFilterTestInstance = Object.create(KriegslustigFilter)
+  kriegslustigFilterTestInstance.collection = TestCollection
+  kriegslustigFilterTestInstance.init()
+  TestCollection.insert({
+    something: true
+  })
+  test.equal(kriegslustigFilterTestInstance.getItems()[0].something, true, 'Initiation failed');
 });
